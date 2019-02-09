44429e00414fdc424932acaf054a7ffeee3366b8
Carter Watts <cdwlions2016@gmail.com>
2019-02-08 19:00:04 -0500

    First I2C attempt

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.I2C.Port;
 
 public class Robot extends TimedRobot {
 
@@ -12,13 +14,14 @@ public class Robot extends TimedRobot {
     public static Drivetrain m_drivetrain;
 	Command m_autonomousCommand;
 	SendableChooser<Command> m_chooser = new SendableChooser<>();
+    private static I2C Wire;
 
 	@Override
 	public void robotInit() {
-		m_oi = new OI();
+        m_oi = new OI();
 		RobotMap.init();
         m_drivetrain = new Drivetrain();
-		
+        Wire = new I2C(Port.kOnboard, 4);
 	}
 
     @Override
@@ -52,6 +55,9 @@ public class Robot extends TimedRobot {
         if (m_autonomousCommand != null) {
             m_autonomousCommand.cancel();
         }
+
+        byte[] command = new byte[1];
+        Wire.transaction(command, command.length, null, 0);
     }
 
     @Override

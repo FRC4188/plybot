89c3fa8a95a20e38f3762f45048a637603a2445b
Locke Adams <lockeadams@gmail.com>
2020-01-12 15:12:09 -0500

    Rewrote codebase for 2020. * Uses TimedRobot framework instead of Command Based. * Has code to run sparks and neos off joysticks + solenoid off buttons.

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -2,10 +2,14 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
 
+/**
+ * Don't touch this file.
+ */
 public final class Main {
 
     private Main() {
     }
+
     public static void main(String... args) {
         RobotBase.startRobot(Robot::new);
     }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,62 +1,89 @@
 package frc.robot;
 
-import frc.robot.subsystems.Drivetrain;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 
+/**
+ * The VM is configured to automatically run this class, and to call the functions
+ * corresponding to each mode, as described in the TimedRobot documentation.
+ */
 public class Robot extends TimedRobot {
 
-    public static OI m_oi;
-    public static Drivetrain m_drivetrain;
-	Command m_autonomousCommand;
-	SendableChooser<Command> m_chooser = new SendableChooser<>();
+    // talon initialization
+    private WPI_TalonSRX talon1 = new WPI_TalonSRX(1);
+    private WPI_TalonSRX talon2 = new WPI_TalonSRX(2);
+    private WPI_TalonSRX talon3 = new WPI_TalonSRX(3);
+    private WPI_TalonSRX talon4 = new WPI_TalonSRX(4);
 
-	@Override
-	public void robotInit() {
-		m_oi = new OI();
-		RobotMap.init();
-        m_drivetrain = new Drivetrain();
-		
-	}
+    // spark intialization
+    private CANSparkMax spark1 = new CANSparkMax(11, MotorType.kBrushless);
+    private CANSparkMax spark2 = new CANSparkMax(12, MotorType.kBrushless);
+    private CANSparkMax spark3 = new CANSparkMax(14, MotorType.kBrushless);
+    private CANSparkMax spark4 = new CANSparkMax(11, MotorType.kBrushless);
+
+    // solenoid initialization
+    private DoubleSolenoid solenoid = new DoubleSolenoid(0, 1);
+
+    // controller initialization
+    private XboxController controller = new XboxController(0);
+
+    @Override
+    public void robotInit() {
+
+        // talon configuration
+        talon1.setInverted(false);
+        talon2.setInverted(false);
+        talon3.setInverted(false);
+        talon4.setInverted(false);
+
+        // spark configuration
+        spark1.setInverted(false);
+        spark2.setInverted(false);
+        spark3.setInverted(false);
+        spark4.setInverted(false);
+    
+    }
 
     @Override
     public void robotPeriodic() {
     }
 
     @Override
-    public void disabledInit() {
-    }
-
-    @Override
-    public void disabledPeriodic() {
-        Scheduler.getInstance().run();
-    }
-
-    @Override
     public void autonomousInit() {
-        m_autonomousCommand = m_chooser.getSelected();
-        if (m_autonomousCommand != null) {
-            m_autonomousCommand.start();
-        }
     }
 
     @Override
     public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
-    }
-
-    @Override
-    public void teleopInit() {
-        if (m_autonomousCommand != null) {
-            m_autonomousCommand.cancel();
-        }
     }
 
     @Override
     public void teleopPeriodic() {
-        Scheduler.getInstance().run();
+
+        // set talons to left stick Y
+        double leftY = controller.getY(Hand.kLeft) * -1;
+        talon1.set(leftY);
+        talon2.set(leftY);
+        talon3.set(leftY);
+        talon4.set(leftY);
+
+        // set sparks to right stick Y
+        double rightY = controller.getY(Hand.kRight) * -1;
+        spark1.set(rightY);
+        spark2.set(rightY);
+        spark3.set(rightY);
+        spark4.set(rightY);
+
+        // open solenoid on A, close on B
+        if (controller.getAButtonPressed()) solenoid.set(Value.kForward);
+        else if (controller.getBButtonPressed()) solenoid.set(Value.kReverse);
+        else solenoid.set(Value.kOff);
+
     }
 
     @Override
